510aa15d442b4cbcc821292d595227c20a5275aa
deepak1suyal <dsuyal800@gmail.com>
2024-08-18 22:18:06 +0530

    added vercel

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,10 +1,9 @@
-<script>
-    export let data
+<script lang="ts">
+    export let data;
 </script>
 
-<h1 class=" font-bold text-red-500">Vist <a href="https://kit.svelte.dev">
-    {data.content}
-</h1>
-
-
-
+<div>
+  {#each data.content as { name } }
+    <div class="font-semibold">{name}</div>
+  {/each}
+</div>
